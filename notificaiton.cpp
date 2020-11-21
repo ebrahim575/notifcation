22f70f4514dd9200712082ad81493b9e1602b824
tobeysnake <72365795+ebrahim575@users.noreply.github.com>
2020-11-20 22:19:06 -0600

    notification file

---
+++ notificaiton.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+ //cout << "\n\n\n\n\n\n\t\t\t\t\t\t\tMotion Detected\n\n\n\n\n\n" << endl;
+ Beep(400, 450);
+	//system("pause");
+}
